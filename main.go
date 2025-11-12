074ff88318044c648a2b8eff9d442d9e93852911
Seno Dwi Prasetyo <senothoha77@gmail.com>
2025-11-12 13:16:14 +0700

    test parsing data
    postman POST /todo

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net/http"
-
+	"fmt"
+	"encoding/json"
 	"github.com/senodp/go-rest-api/database"
 	"github.com/labstack/echo"
 )
+
+type CreateRequest struct{
+	Title 			string `json:"title"`
+	Description 	string `json:"description"`
+}
 
 func main(){
 	db := database.InitDb()
@@ -17,6 +23,16 @@ func main(){
 	}
 
 	e := echo.New()
+
+	e.POST("/todo", func(ctx echo.Context)error{
+
+		var request CreateRequest
+		json.NewDecoder(ctx.Request().Body).Decode(&request)
+
+		fmt.Println(request)
+
+		return ctx.String(http.StatusOK, "OK")
+	})
 
 	e.Start(":8080")
 }
