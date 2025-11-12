9c4c1a575a875c84c296e410ff8552dddd6ea4a2
Seno Dwi Prasetyo <senothoha77@gmail.com>
2025-11-12 14:11:54 +0700

    endpoint list data from db

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ type CreateRequest struct{
 	Description 	string `json:"description"`
 }
 
+type TodoResponse struct{
+	Id 				int 	`json:"id"`
+	Title 			string 	`json:"title"`
+	Description 	string 	`json:"description"`
+	Done 			bool 	`json:"bool"`
+}
+
 func main(){
 	db := database.InitDb()
 	defer db.Close()
@@ -23,6 +30,41 @@ func main(){
 	}
 
 	e := echo.New()
+
+	e.GET("/todo", func(ctx echo.Context)error{
+		rows, err := db.Query("SELECT * FROM todolist")
+
+		if err != nil{
+			return ctx.String(http.StatusInternalServerError, err.Error())
+		}
+
+		var res []TodoResponse
+
+		for rows.Next(){
+			var id int 
+			var title string
+			var description string
+			var done int
+
+			err = rows.Scan(&id, &title, &description, &done)
+
+			if err != nil{
+			return ctx.String(http.StatusInternalServerError, err.Error())
+			}
+
+			var todo TodoResponse
+			todo.Id = id
+			todo.Title = title
+			todo.Description = description
+			if done == 1{
+				todo.Done = true
+			}
+
+			res = append(res, todo)
+		}
+
+		return ctx.JSON(http.StatusOK, res)
+	})
 
 	e.POST("/todo", func(ctx echo.Context)error{
 
